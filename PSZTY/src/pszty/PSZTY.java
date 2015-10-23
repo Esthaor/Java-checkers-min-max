5210b931b0caf5327039f83641523f33c338f950
Grzegorz Majchrzak <gmajchrz@mion.elka.pw.edu.pl>
2015-10-23 19:04:56 +0200

    #1 Zmienione

--- PSZTY/src/pszty/PSZTY.java
+++ PSZTY/src/pszty/PSZTY.java
@@ -15,7 +15,7 @@ public class PSZTY {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("LoL");
     }
     
 }
