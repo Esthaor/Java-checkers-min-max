d7eb7a2f2fd08f0c48cfcdd86c9f792e07311c9c
jedrek18 <stilo001@gmail.com>
2016-01-14 02:25:05 +0100

    #31 Wyświetlanie figur

--- PSZTY/src/pl/pszty/checkers/gui/BoardRenderer.java
+++ PSZTY/src/pl/pszty/checkers/gui/BoardRenderer.java
@@ -14,6 +14,7 @@ import java.awt.image.BufferedImage;
 
 /**
  * Created by jedrek on 13.01.16.
+ * Klasa GUI
  */
 
 public class BoardRenderer extends JFrame implements MouseListener, MouseMotionListener {
@@ -23,20 +24,21 @@ public class BoardRenderer extends JFrame implements MouseListener, MouseMotionL
     JPanel board;
     JLabel figure;
     private Image[][] figureIcons = new Image[2][2];
-
+    ImageIcon blackPawn;
+    ImageIcon whitePawn;
+    ImageIcon blackQueen;
+    ImageIcon whiteQueen;
 
     public BoardRenderer() {
 
         getIcons();
         drawBoard();
+        drawFigures();
         this.setResizable(false);
         this.pack();
         this.setLocationRelativeTo(null);
         this.setVisible(true);
 
-        gameboard = Gameboard.getInstance();
-        Board copyOfOfficialBoard = gameboard.getCoppyOfOfficialBoard();
-        FieldState[][] fields = copyOfOfficialBoard.getBoard();
     }
 
     public void getIcons() { //pobranie ikon z pliku png
@@ -52,6 +54,10 @@ public class BoardRenderer extends JFrame implements MouseListener, MouseMotionL
             e.printStackTrace();
             System.exit(1);
         }
+        blackPawn = new ImageIcon(figureIcons[0][0]);
+        whitePawn = new ImageIcon(figureIcons[0][1]);
+        blackQueen = new ImageIcon(figureIcons[1][0]);
+        whiteQueen = new ImageIcon(figureIcons[1][1]);
     }
 
     public void drawBoard() {
@@ -73,14 +79,37 @@ public class BoardRenderer extends JFrame implements MouseListener, MouseMotionL
             for (int j = 0; j < 8; j++) {
                 JPanel field = new JPanel(new BorderLayout());
                 if ((i + j) % 2 == 0) {
-                    field.setBackground(Color.white);
+                    field.setBackground(Color.LIGHT_GRAY);
                 } else {
-                    field.setBackground(Color.black);
+                    field.setBackground(Color.DARK_GRAY);
                 }
                 board.add(field);
             }
         }
+    }
 
+    public void drawFigures() {
+        gameboard = Gameboard.getInstance();
+        Board copyOfOfficialBoard = gameboard.getCoppyOfOfficialBoard();
+        FieldState[][] fields = copyOfOfficialBoard.getBoard();
+        for (int i = 0; i < 8; i++)
+            for (int j = 0; j < 8; j++) {
+                if (fields[i][j].equals(FieldState.empty)) {
+                    continue;
+                } else if (fields[i][j].equals(FieldState.blackPawn)) {
+                    figure = new JLabel(blackPawn);
+                } else if (fields[i][j].equals(FieldState.whitePawn)) {
+                    figure = new JLabel(whitePawn);
+
+                } else if (fields[i][j].equals(FieldState.blackQueen)) {
+                    figure = new JLabel(blackQueen);
+
+                } else if (fields[i][j].equals(FieldState.whiteQueen)) {
+                    figure = new JLabel(whiteQueen);
+                }
+                JPanel panel = (JPanel) board.getComponent(i * 8 + j);
+                panel.add(figure);
+            }
     }
 
 
